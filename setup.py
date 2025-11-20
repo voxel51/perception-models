3d232d874b4a2d73e5fba8f0b6882409a0c15011
Jacob Sela <jacob.sela@voxel51.com>
2025-11-20 14:11:02 -0800

    lower python req to 3.9

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
         "License :: Other/Proprietary License",
     ],
     license="FAIR Noncommercial Research License",
-    python_requires=">=3.11",
+    python_requires=">=3.9",
     include_package_data=True,
 )
