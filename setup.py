b9f01843b48defa0607e61673e33d9c1ec4b5505
Alex Foley <alex@voxel51.com>
2025-11-25 20:10:59 +0000

    chore(setup.py): Bump setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="perception_models",
-    version="1.0.0",
+    version="1.0.0.dev1",
     author="Meta AI Research, FAIR",
     description="Models of the Perception family.",
     url="https://github.com/facebookresearch/perception_models",
